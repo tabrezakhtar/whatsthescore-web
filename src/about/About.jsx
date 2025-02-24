e36e19b29efb873105f44482a3951ee3f47cd568
tabrezakhtar <tabrez.akhtar1@gmail.com>
2025-02-24 14:00:49 +0000

    add responsive menus

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -5,13 +5,12 @@ function About() {
         A simple score keeper app for forgetful tennis players!
       </p>
 
-      <p className="description">Built with React.</p>
+      <p className="description">Built with React and BeerCSS.</p>
 
       <p>Icons made by:</p>
       <p><a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> 
       &nbsp;from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
       <p>Fonts:</p>
-      <p><a href="https://www.dafont.com/the-led-display-st.font">The Led Display St</a></p>
       <p><a href="https://fonts.google.com/specimen/Roboto">Roboto</a></p>
     </div>
   )
--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter, NavLink, Switch, Route } from "react-router-dom";
 import App from "./App";
 import Matches from "./matches/Matches";
 import About from "./about/About";
-import Menu from "./menu/Menu";
-import Hamburger from "./menu/Hamburger";
 import { MatchesProvider } from "./context/matchesContext";
 import { StopWatchProvider } from "./context/stopWatchContext";
 import { MenuProvider } from "./context/menuContext";
@@ -27,8 +25,8 @@ createRoot(document.getElementById("root")).render(
         <StopWatchProvider>
           <MenuProvider>
             <main className="responsive">
-              <header className="fixed">
-                <nav>
+              <header className="fixed no-padding">
+                <nav className="l">
                   <NavLink to="/"><img className="circle" src="/ball.png"/></NavLink>
                   <h5 className="page right active no-margin l">whatsthescore</h5>
                   <div className="max"></div>
@@ -40,16 +38,56 @@ createRoot(document.getElementById("root")).render(
                   </NavLink>
                   <NavLink exact to="/matches">
                     <button className="small-round large transparent no-margin l">
-                    <i className="fas fa-camera"></i>
+                    <i className="fas fa-clipboard-list"></i>
                       <span>Matches</span>
                     </button>
                   </NavLink>
                   <NavLink exact to="/About">
                     <button className="small-round large transparent no-margin l">
-                      <i className="fas fa-camera"></i>
+                      <i className="fas fa-circle-user"></i>
                       <span>About</span>
                     </button>
-                  </NavLink>                  
+                  </NavLink>
+                </nav>
+
+                <nav className="m no-margin padding">
+                  <NavLink to="/"><img className="circle" src="/ball.png"/></NavLink>
+                  <h5 className="page right active no-margin l">whatsthescore</h5>
+                  <div className="max"></div>
+                  <NavLink exact to="/">
+                    <i className="fa-solid fa-house"></i>
+                  </NavLink>
+                  <NavLink exact to="/matches">
+                    <i className="fas fa-clipboard-list"></i>
+                  </NavLink>
+                  <NavLink exact to="/About">
+                      <i className="fas fa-circle-user"></i>
+                  </NavLink>
+                </nav>
+
+                <nav className="s no-margin padding">
+                  <button className="circle large transparent s" data-ui="#menu">
+                    <i>menu</i>
+                    <menu id="menu" className="no-wrap">
+                      <li>
+                        <NavLink exact to="/" data-ui="#menu">
+                          Home
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink exact to="/matches" data-ui="#menu">
+                          Matches
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink exact to="/About" data-ui="#menu">
+                          About
+                        </NavLink>
+                      </li>
+                    </menu>
+                  </button>
+                  <NavLink to="/"><img className="circle" src="/ball.png"/></NavLink>
+                  <h5 className="page right active no-margin l">whatsthescore</h5>
                 </nav>
               </header>
 
