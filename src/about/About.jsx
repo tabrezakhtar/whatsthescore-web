a17823d770659a0ad8bf3e85d75765c0032543e7
tabrezakhtar <tabrez.akhtar1@gmail.com>
2025-03-02 22:30:16 +0000

    style links

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -19,12 +19,13 @@ function About() {
         <p className="description">Built with React and BeerCSS.</p>
 
         <p>Icons:</p>
-        <p><a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> 
+        <p><a class="link no-round yellow" href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> <a class="link no-round yellow" href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> 
         &nbsp;from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
 
-        <a href="https://www.freepik.com/icon/tennis_9012192#fromView=search&page=1&position=43&uuid=6655a22b-39e8-4616-a958-4ab9d5ebe7ac">Tennis Icon</a>
+        <a class="link no-round yellow" href="https://www.freepik.com/icon/tennis_9012192#fromView=search&page=1&position=43&uuid=6655a22b-39e8-4616-a958-4ab9d5ebe7ac">Tennis Icon</a>
 
-        <p>Fonts: <a href="https://fonts.google.com/specimen/Roboto">Roboto</a></p>
+        <p>Tennis background from <a class="link no-round yellow" href="https://unsplash.com/photos/green-tennis-ball-on-brown-soil-eE4hMhVmS1M">Unsplash</a></p>
+        <p>Fonts: <a class="link no-round yellow" href="https://fonts.google.com/specimen/Roboto">Roboto</a></p>
       </div>
     </>
   )
